ec76a178e7a1e2664355f13a0058256f1979e973
Stiefanek <107305979+Stiefanek@users.noreply.github.com>
2022-10-09 19:25:39 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+﻿namespace MinerMonkey
+{
+    public static class ModHelperData
+    {
+        public const string Version = "1.1.0";
+        public const string Name = "MinerMonkey";
+        public const string Description =
+            "The crazy scientist Pyromanic developed the Miner Monkey to perfection.";
+
+        public const string RepoOwner = "Stiefanek";
+        public const string RepoName = "MinerMonkey";
+        public const string Icon = "MinerPickDisplay.png";
+    }
+}
